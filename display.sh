54611bdfd6407831f21d00fe798a81d54803300e
Guo Yong <wolfg1969@gmail.com>
2012-10-05 19:14:02 +0800

    disable dmis; call fmc once

--- display.sh
+++ display.sh
@@ -7,16 +7,15 @@ stty 9600 -echo < /dev/ttyATH0
 old_name=""
 while true
 do
-    name=$(fmc -a localhost -p 10098 info | sed -n 2p | awk -F- '{print $1;}' | sed 's/^ *//; s/; */;/g')
+    info=$(fmc -a localhost -p 10098 info | sed -n 2p)
+    name=$(echo $info | awk -F- '{print $1;}' | sed 's/^ *//; s/; */;/g')
     if [ "$name" != "$old_name" ]; then
     	echo "fmbox clr" > /dev/ttyATH0
     	old_name=$name
-    	
+    	title=$(echo $info | awk -F- '{print $2;}' | sed 's/^ *//; s/; */;/g')
     	echo "fmbox singer name^$name" > /dev/ttyATH0
-    	#echo "fmbox singer^$name" >> /tmp/display.log
-    
-    	title=$(fmc -a localhost -p 10098 info | sed -n 2p | awk -F- '{print $2;}' | sed 's/^ *//; s/; */;/g')
     	echo "fmbox song name^$title" > /dev/ttyATH0
+    	#echo "fmbox singer^$name" >> /tmp/display.log
     	#echo "fmbox song name^$title" >> /tmp/display.log
     fi
 
--- doubanfm_interface/doubanfm_interface.ino
+++ doubanfm_interface/doubanfm_interface.ino
@@ -1,64 +1,33 @@
+#include <LiquidCrystal.h> 
+#include <Messenger.h>
+#include <SoftwareSerial.h>
+
 /*
  * Douban FM interface
  */
 
-#include <LiquidCrystal.h> 
-#include <Messenger.h>
-
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+SoftwareSerial mySerial(8, 9); // RX, TX
 
 Messenger message = Messenger('^');
 
 // Define the max size of the string
 // The size must be big enough to hold the longest string you are expecting
-#define MAXSIZE 20 
+#define MAXSIZE 17 
 
 // Create a char array (string) to hold the received string
 char inData[MAXSIZE];
-String msg;
 
 boolean fmStart = false;
-int channelValue = 0;
 
-void messageCompleted() {
-  
-  //Serial.print("tunner: ");
-  
-  //Serial.print(channelValue);
-  
-  while (message.available()) {
-    
-    if (message.checkString("fmbox init")) {
-      fmStart = true;
-      lcd.clear();
-      lcd.print("fmbox init");
-    } else {
-      if (fmStart) {
-        boolean skip = false;
-        if (message.checkString("fmbox singer")) { 
-          lcd.clear();        
-          lcd.setCursor(0,1);
-        } else if (message.checkString("fmbox song name")) {          
-          lcd.setCursor(0,0);          
-        } else {
-          skip = true;
-          if (message.checkString("fmbox clr")) {
-            lcd.clear();
-          }
-        }
-        if (!skip) {
-          message.copyString(inData,MAXSIZE); 
-          lcd.print(inData);   
-        }
-      }
-    }
-  }  
-}
+long previousMillis = 0;
+long interval = 3000;
  
 void setup() {
   // initialize serial:
-  Serial.begin(9600);
+  mySerial.begin(9600);
   
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
@@ -72,14 +41,48 @@ void setup() {
 }
 
 void loop() {  
-  // read the input on analog pin 0:
-  channelValue = analogRead(A0);
+  unsigned long currentMillis = millis();
   
-  while (Serial.available()) {    
-    message.process(Serial.read());     
+  while (mySerial.available()) {  
+    message.process(mySerial.read());     
   }
   
-  //Serial.print("tunner: ");
-  //Serial.println(channelValue);
-  //delay(1);
+  if (currentMillis - previousMillis > interval) {
+    previousMillis = currentMillis;
+    // read the input on analog pin 0:
+    int channelValue = analogRead(A0);
+    //if (mySerial.available()) {
+      mySerial.print("tuner: ");
+      mySerial.print(channelValue);
+      mySerial.println();
+    //}
+  }
 }
+
+void messageCompleted() {
+  
+  while (message.available()) {
+    
+    if (message.checkString("fmbox init")) {
+      fmStart = true;
+      lcd.clear();
+      lcd.print("fmbox init");
+    } else {
+      if (fmStart) {
+        if (message.checkString("fmbox singer name")) { 
+          //lcd.clear();        
+          lcd.setCursor(0,1);
+          message.copyString(inData,MAXSIZE); 
+          lcd.print(inData); 
+        } else if (message.checkString("fmbox song name")) {          
+          lcd.setCursor(0,0); 
+          message.copyString(inData,MAXSIZE); 
+          lcd.print(inData);          
+        } else if (message.checkString("fmbox clr")) {
+            lcd.clear();
+        }
+      }
+    }
+  }  
+}
+
